db50bbcfeed0eee1ede669c9006420bd6745df3f
Alex Jurkiewicz <alex@jurkiewi.cz>
2018-03-27 23:45:00 +1100

    Improve generate-github-pages.sh

--- generate-github-pages.sh
+++ generate-github-pages.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -eux
 
+cd "$(dirname $0)"
+rm -rf docs
 ng build --prod --output-path docs --base-href brew-journey
 cp docs/index.html docs/404.html
+echo 'Done, now run git add docs/ and commit'
